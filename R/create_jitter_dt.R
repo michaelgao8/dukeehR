b01fbaee51e90aae805dadadc11dff65fde3def1
michaelgao8 <michaelgao8@gmail.com>
2017-07-20 17:17:40 -0400

    assume lubridate and dataset->vector

--- R/create_jitter_dt.R
+++ R/create_jitter_dt.R
@@ -1,21 +1,20 @@
 #' Create Jittered Date Table
 #'
-#' This function creates a lookup dataframe where one column is the original key of an input dataframe and the other is a random number of days (by patient) 
+#' This function creates a lookup dataframe where one column is the original key of an input dataframe and the other is a random number of days (by patient)
 #' to jitter the dates
-#' @param dataset The input data.frame that contains a column which is the key (unique identifier)
-#' @param keycol_name A string that is the name of the column that the unique id is located in
+#' @param key A vector of all IDs that need to be jittered
 #' @param max_days An integer representing the largest number of days that the dates can be jittered
-#' @param seed The random seed used to generate the dataset. This can be used to reconstruct the id mapping at a later stage. 
+#' @param seed The random seed used to generate the dataset. This can be used to reconstruct the id mapping at a later stage.
 #' @return A data.frame that contains 2 columns named original.ID and dates to jitter
 #' @import lubridate
 #' @export
 
-create_jitter_dt <- function(dataset, keycol_name, max_days = 100, seed = 123){
+create_jitter_dt <- function(key, max_days = 100, seed = 123){
   # Set seed
   set.seed(seed)
-  
+
   #assert(max_days > length(unique(dataset[[keycol_name]])))
-  jdt <- data.frame(unique(dataset[[keycol_name]]), sample(1:max_days, size = length(unique(dataset[[keycol_name]])), replace = TRUE))
-  names(jdt) <- c(keycol_name, "date_jittered")
+  jdt <- data.frame(unique(key), sample(1:max_days, size = length(unique(key)), replace = TRUE))
+  names(jdt) <- c("jdt_key", "date_jittered")
   jdt
 }
--- R/jitter_dates.R
+++ R/jitter_dates.R
@@ -1,10 +1,10 @@
 #' Jitter Dates
 #'
-#' This function creates a lookup dataframe where one column is the original key of an input dataframe and the other is a random number of days (by patient) 
-#' to jitter the dates
+#' This function creates a lookup dataframe where one column is the original key of an input dataframe and the other is a random number of days (by patient)
+#' to jitter the dates. Assumes that all date columns have already been converted to lubridate objects
 #' @param dataset The input data.frame that contains a column which is the key (unique identifier)
 #' @param keycol_name A string that is the name of the column that the unique id is located in dataset
-#' @param date_cols A named vector of strings where the elements are the names of the columns in dataset that are presumed dates. The names of the elements are the lubridate-esque formats of the dates, e.g. "mdy_hms"
+#' @param datecols A character vector that contains all date columns that need to be jittered
 #' @param direction Should the function jitter dates forward or backward? Defaults to "backward"
 #' @param lookup lookup is a 2-column dataframe with the first column being the key and the second column being the number of days to jitter. Usually the output from create_jitter_dt
 #' @return The original dataframe with all dates jittered
@@ -12,55 +12,84 @@
 #' @export
 
 jitter_dates <-
-  function(dataset, keycol_name, date_cols, direction = "backward", lookup){
-    
-    
+  function(dataset, keycol_name, datecols, direction = "backward", lookup){
+
+
     # Convert Dates -----------------------------------------------------
     # TODO: change this to an actual boolean
-    isdate <- numeric(length(date_cols))
-    check_date <- function(x){
-      any(c(is.Date(x), is.POSIXt(x)))
-    }
-    
-    for (i in seq_along(1:length(date_cols))){
-      isdate[i] <- check_date(dataset[[date_cols[i]]])
-    }
-    
-    # isdate now contains a boolean for whether or not each column specified in date_cols is a date already or not
-    # if it is, we can skip it
-    
-    # convert
-    for (j in seq_along(1:length(date_cols))) {
-      if (isdate[j]) {
-        # If this is true, skip
-        next
-      }
-      else{
-        lubr_func <- match.fun(tolower(names(date_cols)[j]))
-        dataset[[date_cols[[j]]]] <- lubr_func(dataset[[date_cols[[j]]]])
-      }
-    }
-    
+    # isdate <- numeric(length(date_cols))
+    # check_date <- function(x){
+    #   any(c(is.Date(x), is.POSIXt(x)))
+    # }
+    #
+    # for (i in seq_along(1:length(date_cols))){
+    #   isdate[i] <- check_date(dataset[[date_cols[i]]])
+    # }
+    #
+    # # isdate now contains a boolean for whether or not each column specified in date_cols is a date already or not
+    # # if it is, we can skip it
+    #
+    # # convert
+    # for (j in seq_along(1:length(date_cols))) {
+    #   if (isdate[j]) {
+    #     # If this is true, skip
+    #     next
+    #   }
+    #   else{
+    #     lubr_func <- match.fun(tolower(names(date_cols)[j]))
+    #     dataset[[date_cols[[j]]]] <- lubr_func(dataset[[date_cols[[j]]]])
+    #   }
+    # }
+    #
     # convert lookup to a named numeric vector
-    datestojitter <- c(lookup[, 2])
-    names(datestojitter) <- as.character(lookup[,1])
-    if(direction=="backward"){
-      for (k in seq_along(1:length(date_cols))) {
-        for (l in seq_along(1:nrow(dataset))) {
-          dataset[[date_cols[k]]][l] <- dataset[[date_cols[k]]][l] - ddays(datestojitter[[dataset[[keycol_name]][[l]]]])
-          
-        }
+    # datestojitter <- c(lookup[, 2])
+    # names(datestojitter) <- as.character(lookup[,1])
+    # if(direction=="backward"){
+    #   for (k in seq_along(1:length(date_cols))) {
+    #     for (l in seq_along(1:nrow(dataset))) {
+    #       dataset[[date_cols[k]]][l] <- dataset[[date_cols[k]]][l] - ddays(datestojitter[[dataset[[keycol_name]][[l]]]])
+    #
+    #     }
+    #   }
+    # }
+    #
+    # if (direction == "forward") {
+    #   for (k in seq_along(1:length(date_cols))) {
+    #     for (l in seq_along(1:nrow(dataset))) {
+    #       dataset[[date_cols[k]]][l] <-
+    #         dataset[[date_cols[k]]][l] + ddays(datestojitter[[dataset[[keycol_name]][[l]]]])
+    #     }
+    #   }
+    # }
+    # return(dataset)
+
+    if (direction == "forward"|direction == "forwards"){
+      # join by
+      jby <- c("jdt_key")
+      names(jby) <- keycol_name
+      # Join the dataset
+      dataset <- dplyr::left_join(dataset, lookup, by = jby)
+      for (col in datecols){
+        dataset[[col]] <- dataset[[col]] + ddays(dataset[["date_jittered"]])
       }
+      # drop the last column
+      dataset[["date_jittered"]] <- NULL
     }
-    
-    if (direction == "forward") {
-      for (k in seq_along(1:length(date_cols))) {
-        for (l in seq_along(1:nrow(dataset))) {
-          dataset[[date_cols[k]]][l] <-
-            dataset[[date_cols[k]]][l] + ddays(datestojitter[[dataset[[keycol_name]][[l]]]])
-        }
+
+    if (direction == "backward"|direction == "backwards"){
+      # join by
+      jby <- c("jdt_key")
+      names(jby) <- keycol_name
+      # join dataset
+
+      dataset <- dplyr::left_join(dataset, lookup, by = jby)
+      for (col in datecols){
+        dataset[[col]] <- dataset[[col]] - ddays(dataset[["date_jittered"]])
       }
+      # drop the last column
+      dataset[["date_jittered"]] <- NULL
     }
+
     return(dataset)
   }
 
